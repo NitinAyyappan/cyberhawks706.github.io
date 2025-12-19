5578602a5efce339bcd60fa7b0af5eb9c2c4030d
SpyroStudios <156618508+SpyroStudios@users.noreply.github.com>
2025-12-18 18:15:46 -0600

    image fixes

--- scripts/include.js
+++ scripts/include.js
@@ -1,10 +1,5 @@
 document.querySelectorAll('[data-include]').forEach(el => {
   fetch(el.getAttribute('data-include'))
-    .then(response => response.text())
-    .then(html => {
-      el.innerHTML = html;
-      const event = new Event('includesLoaded');
-      window.__includesLoaded = true;
-      document.dispatchEvent(event);
-    });
+    .then(resp => resp.text())
+    .then(html => { el.innerHTML = html; });
 });
